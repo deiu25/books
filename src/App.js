f9b6d0de9d3d2422e17a49c9df15aa0667608e1b
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-05 12:53:10 +0300

    setup the server, db, api, create new record

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import 'bulma/css/bulma.min.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+import "bulma/css/bulma.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 import IdeaCreate from "./components/IdeaCreate";
 import IdeaList from "./components/IdeaList";
-import { ThemeContext } from './components/ThemeContext';
+import { ThemeContext } from "./components/ThemeContext";
+import axios from "axios";
 
 function App() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  const appClass = theme === 'light' ? 'app light' : 'app dark';
+  const appClass = theme === "light" ? "app light" : "app dark";
 
-  const [ideas, setIdeas] = useState([])
+  const [ideas, setIdeas] = useState([]);
 
   useEffect(() => {
-    const allIdeas = Array.from({length: 100}, (_, i) => ({ id: i+1, title: `Idea ${i+1}` }));
-  
+    const allIdeas = Array.from({ length: 100 }, (_, i) => ({
+      id: i + 1,
+      title: `Idea ${i + 1}`,
+    }));
+
     const randomIdeas = [];
-  
+
     for (let i = 0; i < 4; i++) {
       const randomIdea = allIdeas[Math.floor(Math.random() * allIdeas.length)];
-      if (!randomIdeas.find(idea => idea.id === randomIdea.id)) {
+      if (!randomIdeas.find((idea) => idea.id === randomIdea.id)) {
         randomIdeas.push(randomIdea);
       } else {
         i--;
       }
     }
-  
+
     setIdeas(randomIdeas);
   }, []);
 
   const editIdeaById = (id, newtitle) => {
     const updatedIdeas = ideas.map((idea) => {
-      if(idea.id === id) {
+      if (idea.id === id) {
         return { ...idea, title: newtitle };
       } else {
         return idea;
@@ -48,15 +52,22 @@ function App() {
     setIdeas(updatedIdeas);
   };
 
-  const createIdea = (title) => {
-    const updatedIdeas = [...ideas, { id: Math.round(Math.random() * 9999 ), title } ];
+  const createIdea = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", { title });
+
+    const updatedIdeas = [...ideas, response.data ];
        setIdeas(updatedIdeas);
   };
 
   return (
     <div className={appClass}>
-      <button className={`theme-button button ${theme === 'light' ? 'is-info' : 'is-dark'}`} onClick={toggleTheme}>
-        {theme === 'light' ? (
+      <button
+        className={`theme-button button ${
+          theme === "light" ? "is-info" : "is-dark"
+        }`}
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? (
           <span className="icon">
             <i className="fas fa-moon"></i>
           </span>
@@ -68,14 +79,16 @@ function App() {
       </button>
       <div className="container mt-5">
         <h1 className="title has-text-centered">Ideas</h1>
-        <hr className="custom-hr"/>
+        <hr className="custom-hr" />
         <IdeaCreate onCreate={createIdea} />
-        <IdeaList onEdit={editIdeaById} ideas={ideas} onDelete={deleteIdeaById}/>
+        <IdeaList
+          onEdit={editIdeaById}
+          ideas={ideas}
+          onDelete={deleteIdeaById}
+        />
       </div>
     </div>
   );
-  
-
-  }
+}
 
 export default App;
